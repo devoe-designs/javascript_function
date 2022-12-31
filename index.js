832acb1b0a7d23e508a4d77265501cc645a7987f
Cierra DeVoe <cierradevoe@gmail.com>
2022-12-31 14:02:49 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const firstName = "Cierra";
+const height = "5'3";
+const country = "USA";
+
+function displayInfo (firstName, height, country) {
+    alert (firstName + " " + height + " " + country);
+}
+
+displayInfo("Cierra", "5'3", "USA");
